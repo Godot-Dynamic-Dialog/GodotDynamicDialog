7ebe4f959d2bca57ff65a960a56e713caee39785
Robie Karuzas <robie.karuzas@gmail.com>
2023-11-05 12:09:11 -0800

    Merging previous commits from dev branch to main

--- Database/database.py
+++ Database/database.py
@@ -15,27 +15,49 @@ password = os.getenv('DATABASE_PASSWORD')
 connection_string = f'DRIVER={driver};SERVER={server};DATABASE={database};UID={username};PWD={password}'
 
 # Connect to your database
-conn = pyodbc.connect(connection_string)
-cursor = conn.cursor()
+#conn = pyodbc.connect(connection_string)
+#cursor = conn.cursor()
 
 # SQL command to INSERT a row into promptHistory
 insert_command = '''
-INSERT INTO promptHistory (userId, promptText, createdAt)
-VALUES (?, ?, GETDATE());
+INSERT INTO promptHistory (userId, promptText, createdAt, gptResponse)
+VALUES (?, ?, GETDATE(), ?);
 '''
 
 # Data to insert
-user_id = 1
-prompt_text = 'This is from the python file!'
+#user_id = 21
+#prompt_text = 'entry from rob'
 
-# Execute the INSERT command
-cursor.execute(insert_command, (user_id, prompt_text))
+#Execute the INSERT command
+#cursor.execute(insert_command, (user_id, prompt_text))
+
+
+#Database execute function
+def executeFunction(id, text, answer):
+
+    # Connect to your database
+    conn = pyodbc.connect(connection_string)
+    cursor = conn.cursor()
+
+    # Execute the INSERT command
+    cursor.execute(insert_command, (id, text, answer))
+    print(answer)
+
+    # Commit the transaction
+    conn.commit()
+
+    # Close the cursor and connection
+    cursor.close()
+    conn.close()
+
+    return "Data inserted successfully!"
+
 
 # Commit the transaction
-conn.commit()
+#conn.commit()
 
 # Close the cursor and connection
-cursor.close()
-conn.close()
+#cursor.close()
+#conn.close()
 
-print("Data inserted successfully!")
+#print("Data inserted successfully!")
