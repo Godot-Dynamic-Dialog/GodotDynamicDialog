354a8e17e98cb382a45a4c6e35bb297c19dedcc4
Callum Brezden <callumbrezden@gmail.com>
2023-11-17 18:01:42 -0800

    Player Animation

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,5 +1,5 @@
 extends CharacterBody2D
-
+@onready var AnimatedSprite = $Sprite
 
 const MAX_SPEED = 65
 const ACCELERATION = 600
@@ -9,6 +9,7 @@ var axis = Vector2.ZERO
 
 func _physics_process(delta):
 	move(delta)
+	animate()
 
 func get_input_axis():
 	axis.x = int(Input.is_action_pressed("move_right")) - int(Input.is_action_pressed("move_left"))
@@ -27,3 +28,23 @@ func move(delta):
 		velocity += (axis * ACCELERATION * delta)
 		velocity = velocity.limit_length(MAX_SPEED)
 	move_and_slide()
+	
+func animate():
+	if axis != Vector2.ZERO:
+		# Determine the primary direction of motion
+		if abs(axis.x) > abs(axis.y):
+			# Moving more horizontally
+			if axis.x > 0:
+				AnimatedSprite.play("right")
+			else:
+				AnimatedSprite.play("left")
+		else:
+			# Moving more vertically
+			if axis.y > 0:
+				AnimatedSprite.play("forward")
+			else:
+				AnimatedSprite.play("back")
+	else:
+		# No movement, stop the animation on the current frame
+		AnimatedSprite.stop()
+		AnimatedSprite.frame = 1	
