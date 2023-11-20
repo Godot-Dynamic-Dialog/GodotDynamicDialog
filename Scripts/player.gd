f22ed8e8108e2b4f451f08ccf799e49eec3ce6a2
Callum Brezden <callumbrezden@gmail.com>
2023-11-19 18:52:32 -0800

    Player Animation Tree Setup

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,6 +1,7 @@
 extends CharacterBody2D
 @onready var animation_player = $AnimationPlayer
-
+@onready var animation_tree = $AnimationTree
+@onready var animation_state = animation_tree.get("parameters/playback")
 
 @export var MAX_SPEED = 65
 @export var ACCELERATION = 600
@@ -10,11 +11,7 @@ var axis = Vector2.ZERO
 var player_attacking = false
 
 func _physics_process(delta):
-	if (Input.is_action_just_pressed("attack")):
-		print("Attack")
-	elif (!player_attacking):
-		move(delta)
-		animate()
+	move(delta)
 
 func get_input_axis():
 	axis.x = int(Input.is_action_pressed("move_right")) - int(Input.is_action_pressed("move_left"))
@@ -25,32 +22,15 @@ func move(delta):
 	axis = get_input_axis()
 	
 	if (axis == Vector2.ZERO):
+		animation_state.travel("Idle")
 		if (velocity.length() > (FRICTION * delta)):
 			velocity -= velocity.normalized() * (FRICTION * delta)
 		else:
 			velocity = Vector2.ZERO
 	else: 
+		animation_tree.set("parameters/Idle/blend_position", axis)
+		animation_tree.set("parameters/Run/blend_position", axis)
+		animation_state.travel("Run")
 		velocity += (axis * ACCELERATION * delta)
 		velocity = velocity.limit_length(MAX_SPEED)
 	move_and_slide()
-	
-func animate():
-	if axis != Vector2.ZERO:
-		# Determine the primary direction of motion
-		if abs(axis.x) > abs(axis.y):
-			# Moving more horizontally
-			if axis.x > 0:
-				animation_player.play("right")
-			else:
-				animation_player.play("left")
-		else:
-			# Moving more vertically
-			if axis.y > 0:
-				animation_player.play("forward")
-			else:
-				animation_player.play("back")
-	else:
-		pass
-		# No movement, stop the animation on the current frame
-		#AnimatedSprite.stop()
-		#AnimatedSprite.frame = 0	
