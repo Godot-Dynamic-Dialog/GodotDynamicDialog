c72e14c36b247b9e1e8a3815e26a96e2072cbc9f
mcnerv <jake.a.pott55@gmail.com>
2023-12-01 10:34:38 -0800

    tracks position now
    woo!

--- Scenes/CaveEntrance.gd
+++ Scenes/CaveEntrance.gd
@@ -4,5 +4,6 @@ extends Area2D
 func _on_body_shape_entered(body_rid, body, body_shape_index, local_shape_index):
 	if "Player" in body.name:
 		print("Cave Entrance Entered")
-		
+		DialogueManager.player_location = get_node("../Player").position
+		print(DialogueManager.player_location)
 		get_tree().change_scene_to_file("res://Scenes/Cave.tscn")
