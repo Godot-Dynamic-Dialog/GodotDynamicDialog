80288362a18ce182da9b8e3157da177409445c08
mcnerv <jake.a.pott55@gmail.com>
2023-12-01 09:21:12 -0800

    minor change
    wanted to do this before I merge branches

--- Scenes/CaveEntrance.gd
+++ Scenes/CaveEntrance.gd
@@ -4,4 +4,5 @@ extends Area2D
 func _on_body_shape_entered(body_rid, body, body_shape_index, local_shape_index):
 	if "Player" in body.name:
 		print("Cave Entrance Entered")
+		
 		get_tree().change_scene_to_file("res://Scenes/Cave.tscn")
