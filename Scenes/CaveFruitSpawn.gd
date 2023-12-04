9564120430f99ce5bcf3c5d09af526b0f2cf40a9
mcnerv <jake.a.pott55@gmail.com>
2023-12-04 14:27:13 -0800

    Added Bat and Cave Fruit
    Animations are a little off, not too sure how to fix that tbh. Fruit also spawn in the middle of the cave, and hopefully I didn't break the prompt input stuff by instancing it as a scene.

---
+++ Scenes/CaveFruitSpawn.gd
@@ -0,0 +1,22 @@
+extends TileMap
+
+#same code as TileMap.gd, just wanted to change coords for where the fruits spawn
+
+var apple = preload("res://Scenes/Objects/apple.tscn")
+var watermelon = preload("res://Scenes/Objects/watermelon.tscn")
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	spawn(10)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+# x = number of object instances to spawn
+func spawn(x):
+	while (x != 0):
+		randomize()
+		var fruits = [apple, watermelon]
+		var kinds = fruits[randi()% fruits.size()]
+		var fruit = kinds.instantiate()
+		fruit.position = Vector2(randi_range(180,350), randi_range(16, -150))
+		fruit.z_index = 1
+		add_child(fruit)
+		x -= 1
---
+++ Scenes/Mobs/Slime/Bat.gd
@@ -0,0 +1,45 @@
+extends CharacterBody2D
+
+@onready var player = $"../Player"
+
+const SPEED = 75.0
+var chase = false
+var player_detect = null
+
+
+func _physics_process(delta):
+	$AnimatedSprite2D.play("forward and idle")
+	if chase:
+		var direction = (player.global_position - global_position).normalized()
+		velocity = direction * SPEED
+		move_and_slide()
+		#play moving right animation
+		if (player.global_position.x > global_position.x):
+			$AnimatedSprite2D.play("right")
+		#play moving backwards
+		elif (player.global_position.y < global_position.y):
+			$AnimatedSprite2D.play("backwards")
+		#play left animation
+		elif (player.global_position.x < global_position.x):
+			$AnimatedSprite2D.play("left")
+		#no need for else statement since forward and idle are same animation
+
+func chasePlayer():
+	chase = true
+
+func stopChase():
+	chase = false
+
+func take_damage():
+	queue_free()
+
+func _on_player_detection_body_entered(body):
+	if body.name == "Player":
+		player_detect = body
+		chasePlayer()
+
+
+func _on_player_detection_body_exited(body):
+	if body.name == "Player":
+		player_detect = null
+		stopChase()
