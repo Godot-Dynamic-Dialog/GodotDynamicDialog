f778ee0e7c98cb578ff79970d1162f0f6befbf85
Braedon McRae <114954354+BraedonM@users.noreply.github.com>
2023-12-04 18:56:22 -0800

    Streamed output & fast response time (#26)
    yerrr

--- Scenes/PromptInput.gd
+++ Scenes/PromptInput.gd
@@ -1,23 +1,76 @@
 extends CanvasLayer
 
 # ChatGPT Init
-var url : String = "https://api.openai.com/v1/chat/completions"
+var host = "https://api.openai.com"
+var path = "/v1/chat/completions"
+var url = host+path
 var temp : float = 0.5  # How varied the responses are
 var max_tokens : int = 200  # Not too long
+var stream : bool = true
 var api_key : String = OS.get_environment("OPENAI_API_KEY")
 var headers = ["Content-type: application/json", 
 	"Authorization: Bearer " + api_key]
 var model : String = "gpt-3.5-turbo"
 # var messages = []  # Only needed for conversations
 var request : HTTPRequest
+signal message_processed(message)
+var message_ai = preload("res://Scenes/chat_message_ai.tscn")
+signal stream_busy(is_busy:bool)
 
+var stream_reply_buffer: String
+var stream_reply_final: String
+var stream_used_status_ai_message = false
+var stream_ongoing = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	# Instantiate HTTP request to GPT
 	request = HTTPRequest.new()
 	add_child(request)
-	request.connect("request_completed", _on_request_completed)
+	#request.connect("request_completed", _on_request_completed)
+	message_processed.connect(_on_request_completed)
+	if stream:
+		$HTTPSSEClient.new_sse_event.connect(_on_new_sse_event)
+
+func _on_new_sse_event(partial_reply : Array, ai_status_message : ChatMessageAI):
+	for string in partial_reply:
+		if string == "[DONE]":
+			$HTTPSSEClient.close_connection()
+			# Whatever is reamining in the buffer, if any, must be sent to the 
+			# RichTextLabel and voice ai service
+			stream_ongoing = false
+			
+			if stream_reply_buffer.length() > 0:
+				stream_reply_final += stream_reply_buffer
+				#_inject_message_from_stream(ai_status_message, stream_reply_buffer) 
+				# We reset the buffer
+				stream_reply_buffer = ""
+				
+			# We append the whole message to our internal chat
+			#chat.append({"role": "assistant", "content":stream_reply_final})
+			# We reset the reply, ready for the next stream
+			stream_reply_final = ""
+			stream_used_status_ai_message = false
+			
+		elif string == "[EMPTY DELTA]":
+			pass
+		elif string == "[ERROR]":
+			$HTTPSSEClient.close_connection()
+			stream_ongoing = false
+			# Whatever is reamining in the buffer, if any, must be sent to the 
+			# RichTextLabel and voice ai service
+			if stream_reply_buffer.length() > 0:
+				stream_reply_final += stream_reply_buffer
+				#_inject_message_from_stream(ai_status_message, stream_reply_buffer) 
+				# We reset the buffer and the reply
+				stream_reply_buffer = ""
+			stream_reply_final = ""
+			stream_used_status_ai_message = false
+		else:
+			# We process the partial reply
+			stream_reply_buffer += string
+			get_node("Label").set_text(stream_reply_buffer)
+			# print("Current buffer: ", stream_reply_buffer)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -54,41 +107,43 @@ only respond with the text of the monologue.
 Stay under 150 characters.
 ")
 
-# NPC Interaction Prompt
-#"
-#You are %s. 
-#Main character (MC) is %s, 
-#%s. %s. 
-#MC's HP is %s%%. 
-#Say something to MC. Your mood is %s. 
-#Don't have to comment on all of the above, 
-#only respond with the text. Stay under 400 characters.
-#")
-
-	#var prompt = promptStruct % [NPC, MC, action, env, hp, mood] # NPC
-	var prompt = promptStruct % [MC, env, hunger, apple, hp, mood]
+	var prompt = promptStruct % [MC, env, hunger, apple, extra, hp, mood]
 	print("Prompt:\n", prompt)
 	
+	var ai_message = message_ai.instantiate()
+	_call_gpt(prompt, ai_message)
+
+
+func _call_gpt(prompt : String, ai_status_message : RichTextLabel) -> void:
+	var headers = [
+		"Content-Type: application/json",
+		"Authorization: Bearer " + api_key
+	]
 	var messages = [{
 		"role": "user",
 		"content": prompt
 	}]
-	
-	var body = JSON.new().stringify({
-		"messages": messages,
-		#"temperature": temp,
-		#"max_tokens": max_tokens,
-		"model": model
+	var body = JSON.stringify({
+		"model": model,
+		"messages": messages, # Send the array to chatGPT
+		"temperature": temp,
+		"stream": stream,
 	})
 	
-	var send_request = request.request(
-		url, headers, HTTPClient.METHOD_POST, body
-		)
+	if stream:
+		$HTTPSSEClient.connect_to_host(host, path, headers, body, ai_status_message, 443)
+		stream_busy.emit(true)
+		stream_ongoing = true
 		
-	# Error handling (removed for speed)
-#	if send_request != OK:
-#		print("Error in ChatGPT request")
-
+	else:
+		var http_request = HTTPRequest.new()
+		add_child(http_request)
+		http_request.request_completed.connect(_on_request_completed)
+		
+		var error = http_request.request(url, headers, HTTPClient.METHOD_POST, body)
+		
+		if error != OK:
+			push_error("Something Went Wrong!")
 
 func _on_request_completed(result, response_code, headers, body):
 	# Get the message string from response
