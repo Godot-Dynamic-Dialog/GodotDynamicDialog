3b68a203ae2b1a3dbab53c85183a0f59fe7f2c33
mcnerv <jake.a.pott55@gmail.com>
2023-12-04 19:19:04 -0800

    Temp Changes
    my json file/dialogue stuff isn't fully updated, pushing this for stability

--- Scenes/ShowDialogueHUD.gd
+++ Scenes/ShowDialogueHUD.gd
@@ -1,2 +1,14 @@
-extends CanvasLayer
+extends Label
 
+var JSON_URL = "res://DialogueManager/dialogues.json"
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+# using this to have a constant update to the values as I do not fully know how to update only when a prompt is changed
+func _process(delta):
+	var file = FileAccess.open(JSON_URL, FileAccess.READ)
+	$Health.text = str(0)
+	$Hunger.text = str(0)
+	$TotalApple.text = str(0)
+	$TotalBanana.text = str(0)
+	$TotalWatermelon.text = str(0)
+	$RainState.text = str(0)
