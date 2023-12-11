cda9f7db8899b932bb683dc97d9d503127f7bdfc
mcnerv <jake.a.pott55@gmail.com>
2023-12-11 15:34:31 -0800

    Do not include quotations

--- Scenes/PromptInput.gd
+++ Scenes/PromptInput.gd
@@ -134,9 +134,9 @@ func _on_gd_gpt_pressed():
 	
 	var prompt = []
 	if (DialogueDatabase.NPC == true):
-		prompt = "You are an NPC storekeep in a video game. Write a voice line based on the following facts about the player character, only respond with the text of the voice line. Stay under 150 characters."
+		prompt = "You are an NPC storekeep in a video game. Write a voice line based on the following facts about the player character, only respond with the text of the voice line. Stay under 150 characters. Do not include quotations."
 	else:
-		prompt = "You are a the main character of a video game. Write a voice line based on the following facts, only respond with the text of the voice line. Stay under 150 characters."
+		prompt = "You are a the main character of a video game. Write a voice line based on the following facts, only respond with the text of the voice line. Stay under 150 characters. Do not include quotations."
 	prompt += DialogueManager.create_prompt()
 		
 	print("Prompt:\n", prompt)
